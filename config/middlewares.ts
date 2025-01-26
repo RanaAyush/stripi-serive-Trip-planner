d56948b0638745d45d3a8f78e08d2e5db237c44b
Ayush Rana <86603886+RanaAyush@users.noreply.github.com>
2025-01-26 22:52:28 +0530

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,15 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'],  // Allow all origins (not recommended for production)
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true,  // Allow credentials if required
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
